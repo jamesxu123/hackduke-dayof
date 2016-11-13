4d9b6e56477bcf7d717b4faddc4f4d8c585512cf
Briguy52 <Briguy52@gmail.com>
2016-11-12 22:32:24 -0500

    basic travel info added to site

---
+++ src/components/FAQItem/FAQItem.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import classes from '../FAQItem/FAQItem.scss'
+
+export const FAQItem = (props) => (
+
+  <div className={classes.section}>
+    <div className={classes.sectionHeader} style={{'textAlign': 'left'}}>
+      {props.title}
+    </div>
+    <div className={classes.sectionText}>
+      {(props.link) ? (
+        generateTextWithLink(props.beforeText, props.link, props.linkText, props.afterText)
+      ) : props.text }
+    </div>
+  </div>
+)
+
+function generateTextWithLink (beforeText, link, linkText, afterText) {
+  return (
+    <div>
+      {beforeText}
+      <a href={link} className={classes.link}>{linkText}</a>
+      {afterText}
+    </div>
+  )
+}
+
+FAQItem.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  text: React.PropTypes.string,
+  beforeText: React.PropTypes.string,
+  afterText: React.PropTypes.string,
+  linkText: React.PropTypes.string,
+  link: React.PropTypes.string
+}
+
+export default FAQItem
--- src/components/TravelInfo/TravelInfo.js
+++ src/components/TravelInfo/TravelInfo.js
@@ -1,16 +1,42 @@
 import React from 'react'
-// import classes from './TravelInfo.scss'
+import classes from './TravelInfo.scss'
 import eventInfoClasses from 'components/EventInfo/EventInfo.scss'
+import FAQItem from 'components/FAQItem'
+import { Col, Row } from 'react-bootstrap'
 
 class TravelInfo extends React.Component {
 
   render () {
     return (
       <div>
-        <div className={eventInfoClasses.container}>
+        <div className={classes.container}>
           <div className={eventInfoClasses.title}>
             TRAVEL INFO
           </div>
+          <Row style={{'paddingTop': '30px'}}>
+            <Col md={6}>
+              <FAQItem title={'Coming from RDU'}
+                beforeText={'If you\'re coming from RDU, you can '}
+                link={'http://charlenesaferide.com/'}
+                linkText={'take a cab to Duke'}
+                afterText={' or call an Uber.'} />
+
+              <FAQItem title={'Finding parking'}
+                beforeText={'If you’re driving over, you can find parking at the '}
+                link={'https://goo.gl/maps/SJ1SxHYGAMn'}
+                linkText={'Duke Chemistry Lot'}
+                afterText={'. We will have a parking attendant from 10 AM - 2PM \
+                           on Saturday. If you will be arriving after that, \
+                           please let us know so we can make appropriate accommodations!'} />
+
+              <FAQItem title={'Getting dropped off'}
+                beforeText={'The easiest place to be dropped off is in front of the Duke Chapel at the '}
+                link={'https://goo.gl/maps/Uh1etuH8sRB2'}
+                linkText={'West Campus bus stop'}
+                afterText={'.'} />
+
+            </Col>
+          </Row>
         </div>
       </div>
     )
@@ -18,4 +44,3 @@ class TravelInfo extends React.Component {
 }
 
 export default TravelInfo
-
