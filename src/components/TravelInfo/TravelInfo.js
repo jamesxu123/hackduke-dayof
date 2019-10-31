c1d6239f6db2083f47d80809e2ef722ee3edddb2
Annie Lu <al343@duke.edu>
2019-10-31 16:13:06 -0400

    travel info

--- src/components/TravelInfo/TravelInfo.js
+++ src/components/TravelInfo/TravelInfo.js
@@ -20,10 +20,10 @@ class TravelInfo extends React.Component {
             <Col md={6}>
               <FAQItem
                 title={'Coming from RDU'}
-                beforeText={"If you're coming from RDU, you can "}
-                link={'http://charlenesaferide.com/'}
-                linkText={'take a cab to Duke'}
-                afterText={' or call an Uber.'}
+                beforeText={"If you're coming from RDU, you can call a Lyft or Uber. Please also submit a request "}
+                link={'https://hackduke.typeform.com/to/qBjLYb'}
+                linkText={'here'}
+                afterText={' for travel reimbursement'}
               />
 
               <FAQItem
@@ -55,140 +55,19 @@ class TravelInfo extends React.Component {
                 }
                 link={'https://docs.google.com/document/d/1eDC1KWeQbYIyWUWhvmuShvsEQsDjROv9GX-6wjDeg_4/edit'}
                 linkText={'here'}
-                afterText={' for more info.'}
+                afterText={' for more info. If you have any more questions, please email hackers@hackduke.org.'}
               />
             </Col>
             <Col md={6}>
               <FAQItem
-                title={'Virginia Tech'}
-                beforeText={'Our VT bus will leave from the '}
+                title={'Buses'}
+                beforeText={"We'll have buses covering the trail from UVA, VTech, Georgia Tech, UNC, and NC State. See detailed itineraries "}
                 link={
-                  'https://www.google.com/maps/place/Squires+Student+Center,+Blacksburg,+VA+24061/@37.2295613,-80.4202095,17z/data=!3m1!4b1!4m5!3m4!1s0x884d95733ac1cb4d:0xf8d9c76521fc0952!8m2!3d37.229557!4d-80.4180208'
+                  'https://docs.google.com/document/d/1GrQsFlQ8waIzlQcexgPJc1iG98hMBvQF4XLhcYYpuIY/edit'
                 }
-                linkText={'Squires Student Center'}
-                afterText={
-                  <span>
-                    <br />
-                    <br />
-                    <u>
-                      <b>Itinerary</b>
-                    </u>
-                    <br />
-                    <b>Saturday, Nov.2 4AM: </b>
-                    Pick up from Squires Student Center
-                    <br />
-                    <b>Saturday, Nov.2 9:30AM: </b>
-                    Arrive at Duke <br />
-                    <b>Sunday, Nov.3 6PM: </b> Pick up from Duke <br />
-                    <br />
-                    Onsite contact:
-                    <b> Sindhu Ranga</b>
-                    <br />
-                    Phone: <b>(571)-258-7059</b>
-                    <br />
-            
-                  </span>
-                }
-              />
-              <FAQItem
-                title={'UVA'}
-                beforeText={'Our UVA bus will leave from the '}
-                link={
-                  'https://www.google.com/maps/place/Scott+Stadium+Parking/@38.0296407,-78.5160005,17.73z/data=!4m5!3m4!1s0x89b3865dd355683d:0xa4d7bec59150eff2!8m2!3d38.0290068!4d-78.5147029'
-                }
-                linkText={'Scott Stadium Parking Lot'}
-                afterText={
-                  <span>
-                    <br />
-                    <br />
-                    <u>
-                      <b>Itinerary</b>
-                    </u>
-                    <br />
-                    <b>Saturday, Nov.2 1AM: </b>
-                    Pick up from Scott Stadium parking lot
-                    <br />
-                    <b>Saturday, Nov.2 9:30AM: </b>
-                    Arrive at Duke <br />
-                    <b>Sunday, Nov.3 6PM: </b> Pick up from Duke <br />
-                    
-                  </span>
-                }
-              />
-              <FAQItem
-                title={'NC State'}
-                beforeText={'Our NC State bus will leave from the '}
-                link={
-                  'https://www.google.com/maps/place/Engineering+Bldg+II,+890+Oval+Dr,+Raleigh,+NC+27606/@35.7720132,-78.6750721,18z/data=!4m5!3m4!1s0x89acf59ddf70cafd:0x47b0abfcfdad99af!8m2!3d35.7719356!4d-78.674002'
-                }
-                linkText={'Engineering Buidling II'}
-                afterText={
-                  <span>
-                    <br />
-                    <br />
-                    <u>
-                      <b>Itinerary</b>
-                    </u>
-                    <br />
-                    <b>Saturday, Nov.2 8:30AM: </b>
-                    Pick up from Engineering Building II 
-                    <br />
-                    <b>Saturday, Nov.2 9:30AM: </b>
-                    Arrive at Duke <br />
-                    <b>Sunday, Nov.3 6PM: </b> Pick up from Duke <br />
-                    
-                  </span>
-                }
-              />
-              <FAQItem
-                title={'Georgia Tech'}
-                beforeText={'Our Georgia Tech bus will leave from the '}
-                link={
-                  'https://www.google.com/maps/place/Georgia+Tech+Student+Center/@33.77386,-84.3995455,18.31z/data=!4m5!3m4!1s0x88f50489a73dcb65:0xe5b99fb6b844e7bb!8m2!3d33.7739778!4d-84.3988123'
-                }
-                linkText={'Student Center'}
-                afterText={
-                  <span>
-                    <br />
-                    <br />
-                    <u>
-                      <b>Itinerary</b>
-                    </u>
-                    <br />
-                    <b>Saturday, Nov.2 1AM: </b>
-                    Pick up from Student Center
-                    <br />
-                    <b>Saturday, Nov.2 9:00AM: </b>
-                    Arrive at Duke <br />
-                    <b>Sunday, Nov.3 6PM: </b> Pick up from Duke <br />
-                    
-                  </span>
-                }
-              />
-              <FAQItem
-                title={'UNC-Chapel Hill'}
-                beforeText={'Our UNC bus will leave from the '}
-                link={
-                  'https://www.google.com/maps/place/Morehead+Planetarium+and+Science+Center/@35.9139076,-79.0526987,17z/data=!3m1!4b1!4m5!3m4!1s0x89acc2c294c7b583:0x8effe7d104029f8c!8m2!3d35.9139033!4d-79.05051'
-                }
-                linkText={'Moorehead Planetarium'}
-                afterText={
-                  <span>
-                    <br />
-                    <br />
-                    <u>
-                      <b>Itinerary</b>
-                    </u>
-                    <br />
-                    <b>Saturday, Nov.2 8:30AM: </b>
-                    Pick up from Moorehead Planetarium
-                    <br />
-                    <b>Saturday, Nov.2 9:00AM: </b>
-                    Arrive at Duke <br />
-                    <b>Sunday, Nov.3 6PM: </b> Pick up from Duke <br />
-                    
-                  </span>
-                }
+                linkText={'here'}
+                afterText={'.'}
+                  
               />
             </Col>
           </Row>
